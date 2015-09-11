55eb9c4b9caeba05b188318a90a494507f1c516f
Fernanda Perez Verdugo <fer20mc@gmail.com>
2015-09-11 17:54:43 -0300

    programa parte 1

---
+++ tarea.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pl
+
+datos=np.loadtxt('sun_AM0.dat')
+x=datos[:,0]
+y=datos[:,1]
+wavel=x*(10**-3)
+flujo=y*(10**10)
+plt.plot(wavel,flujo)
+plt.xlim(0.,6.)
+plt.xlabel('Longitud de onda $[\mu m]$')
+plt.ylabel('Flujo $[erg$ $ s^{-1} cm{-3}]$')
+plt.title('Espectro solar, flujo v/s longitud de onda')
+plt.grid(True)
+plt.savefig('fig1')
+plt.show()
+
